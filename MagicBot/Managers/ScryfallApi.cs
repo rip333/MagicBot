36c130bc50eef9eae512984e01c4175116c2e4af
Rip Britton <ripbritton3@gmail.com>
2021-09-21 14:28:24 -0500

    adding random querying

--- MagicBot/Managers/ScryfallApi.cs
+++ MagicBot/Managers/ScryfallApi.cs
@@ -1,7 +1,6 @@
-using System;
-using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
 using MagicBot.Models.Exceptions;
 using MagicBot.Models.Scryfall;
 using Newtonsoft.Json;
@@ -18,13 +17,25 @@ namespace MagicBot.Managers
 
         public async Task<string> GetCardImageUrlByName(string name)
         {
-            var response = await _httpClient.GetAsync("cards/named?fuzzy=" + name);
-            var jsonString = await response.Content.ReadAsStringAsync();
+            string jsonString;
+            if (name.ToUpper().StartsWith("RANDOM"))
+            {
+                var query = HttpUtility.HtmlDecode(name.ToUpper().Remove(0, 6).Trim());
+                var response = await _httpClient.GetAsync($"cards/random?q={query}");
+                jsonString = await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+                var response = await _httpClient.GetAsync("cards/named?fuzzy=" + name);
+                jsonString = await response.Content.ReadAsStringAsync();
+            }
+
             var card = JsonConvert.DeserializeObject<Card>(jsonString);
-            if (card.Status == 404)
+            if (card.Status == 404 || card.ImageUris.Normal == null)
             {
                 throw new NoCardFoundException();
             }
+
             return card.ImageUris.Normal;
         }
     }
