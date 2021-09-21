c6b2eceed0327de0219d509a1a34a224174e75f3
Rip Britton <ripbritton3@gmail.com>
2021-09-21 14:39:30 -0500

    Update TokenManager.cs

--- MagicBot/Managers/TokenManager.cs
+++ MagicBot/Managers/TokenManager.cs
@@ -2,6 +2,7 @@ namespace MagicBot.Managers
 {
     public static class TokenManager
     {
+        //Replace this with a text file containing your telegram bot's token
         private const string TokenLocation = @"E:\Dev\TelegramBots\MagicBot\token.txt";
         
         public static string Get()
