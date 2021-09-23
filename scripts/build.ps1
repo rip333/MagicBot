0631455baf8f7e7def489c7cbafe726718b03f16
Rip Britton <ripbritton3@gmail.com>
2021-09-22 19:48:53 -0500

    stop process mid deploy

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,5 +1,6 @@
 cd C:\temp\apps\magicbot
 dotnet build --configuration Release
 Compress-Archive -Path C:\temp\apps\magicbot\MagicBot\bin\Release\net5.0 -DestinationPath C:\Users\Administrator\Desktop\MagicBot\magicbot.zip
+Stop-Process -Name "MagicBot"
 cd C:\Users\Administrator\Desktop\MagicBot
 Expand-Archive -Path magicbot.zip
