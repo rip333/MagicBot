eda8ec063756baafe966058303ea1946786e56c9
Rip Britton <ripbritton3@gmail.com>
2021-09-24 17:21:08 -0500

    delete zip folder

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -3,4 +3,5 @@ dotnet build --configuration Release
 Compress-Archive -Path C:\temp\apps\magicbot\MagicBot\bin\Release\net5.0 -DestinationPath C:\Users\Administrator\Desktop\MagicBot\magicbot.zip
 Stop-Process -Name "MagicBot"
 cd C:\Users\Administrator\Desktop\MagicBot
+del C:\Users\Administrator\Desktop\MagicBot\magicbot.zip
 Expand-Archive -Path magicbot.zip
