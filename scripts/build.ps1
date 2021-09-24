149136143b8bd9ee702ba549e26dc566160122d0
Rip Britton <ripbritton3@gmail.com>
2021-09-24 17:24:07 -0500

    really actually doing it this time

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,7 +1,7 @@
 cd C:\temp\apps\magicbot
 dotnet build --configuration Release
+del C:\Users\Administrator\Desktop\MagicBot\magicbot.zip
 Compress-Archive -Path C:\temp\apps\magicbot\MagicBot\bin\Release\net5.0 -DestinationPath C:\Users\Administrator\Desktop\MagicBot\magicbot.zip
 Stop-Process -Name "MagicBot"
 cd C:\Users\Administrator\Desktop\MagicBot
-del C:\Users\Administrator\Desktop\MagicBot\magicbot.zip
 Expand-Archive -Path magicbot.zip
